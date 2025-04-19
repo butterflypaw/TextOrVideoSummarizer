c6843af9e98b1bc20ffd384371ee345680c800e9
butterflypaw <nithya.etoori@gmail.com>
2025-04-19 23:06:07 +0530

    Final

--- WebApp/pages/document.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-import document_data
-import load
-data=document_data.document_extract()
-if data:
-    response=(load.output(data))
-    for chunk in response:
-        st.write(chunk.text)
--- WebApp/pages/url.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-import load
-import website_data
-url=st.text_input("Enter the url",placeholder="URL....")
-if url:
-    response=(load.output(website_data.scrape(url)))
-    for chunk in response:
-        st.write(chunk.text)
